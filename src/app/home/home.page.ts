279b01cd0c661dc4cbc5828a02d26b023a1fe393
Mark Burggraf <markb@mantisbible.com>
2019-03-09 20:32:26 -0800

    added timer

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as moment from 'moment';
+import { start } from 'repl';
 
 @Component({
   selector: 'app-home',
@@ -12,6 +13,9 @@ export class HomePage implements OnInit {
   startDate = moment('1800-01-01');
   endDate = moment('2200-01-01');
   testing = false;
+  startTime = 0;
+  endTime = 0;
+  elapsed = moment.unix(0).utc().format('mm:ss.S');
   buttonColors = [
     'light',
     'light',
@@ -35,29 +39,43 @@ export class HomePage implements OnInit {
 
   ngOnInit() { }
 
+  updateTimerDisplay() {
+    const doUpdate = () => {
+      this.endTime = +new Date();
+      const diff = ((this.endTime - this.startTime) / 1000);
+      this.elapsed = moment.unix(diff).utc().format('mm:ss.S');
+      console.log('this.testing = ' + this.testing);
+      if (this.testing) {
+        setTimeout(function () {
+          doUpdate();
+        }, 100);
+      }
+    };
+    doUpdate();
+  }
+
   test() {
     this.testing = true;
-    console.log('test...');
     for (let i = 0; i < 7; i++) {
       this.buttonColors[i] = 'light';
     }
     this.startDate = moment('1800-01-01');
     const span = this.endDate.diff(this.startDate, 'days');
-    console.log('span: ' + span);
     const rnd = Math.floor(Math.random() * span); // returns a random integer from 0 to 9
     this.startDate.add(rnd, 'days');
     this.TestDisplay = this.startDate.calendar();
+    this.startTime = +new Date();
+    this.updateTimerDisplay();
   }
 
   buttonPress(i: number) {
     if (this.startDate.day() === i) {
       this.buttonColors[i] = 'success';
       this.testing = false;
+      this.updateTimerDisplay();
     } else {
       this.buttonColors[i] = 'danger';
     }
   }
-
-
 
 }
